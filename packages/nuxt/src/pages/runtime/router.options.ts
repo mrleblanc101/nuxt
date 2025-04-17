16f2315164844693bcd72fa2bcde4065b474b32a
Sébastien LeBlanc <mrsebastienleblanc@icloud.com>
2025-04-17 02:36:22 -0400

    fix: scrollBehavior for nested pages

--- packages/nuxt/src/pages/runtime/router.options.ts
+++ packages/nuxt/src/pages/runtime/router.options.ts
@@ -11,44 +11,48 @@ type ScrollPosition = Awaited<ReturnType<RouterScrollBehavior>>
 // Default router options
 // https://router.vuejs.org/api/#routeroptions
 export default <RouterConfig> {
-  scrollBehavior (to, from, savedPosition) {
-    const nuxtApp = useNuxtApp()
+  scrollBehavior(to, from, savedPosition) {
+    const nuxtApp = useNuxtApp();
     // @ts-expect-error untyped, nuxt-injected option
-    const behavior = useRouter().options?.scrollBehaviorType ?? 'auto'
+    const behavior = useRouter().options?.scrollBehaviorType ?? 'auto';
 
-    // By default when the returned position is falsy or an empty object, vue-router will retain the current scroll position
-    // savedPosition is only available for popstate navigations (back button)
-    let position: ScrollPosition = savedPosition || undefined
+    let position: ScrollPosition = savedPosition || undefined;
 
-    const routeAllowsScrollToTop = typeof to.meta.scrollToTop === 'function' ? to.meta.scrollToTop(to, from) : to.meta.scrollToTop
+    const routeAllowsScrollToTop = typeof to.meta.scrollToTop === 'function'
+      ? to.meta.scrollToTop(to, from)
+      : to.meta.scrollToTop;
 
-    // Scroll to top if route is changed by default
+    // Prevent parent scrolling to the top when navigating nested pages
     if (!position && from && to && routeAllowsScrollToTop !== false && isChangingPage(to, from)) {
-      position = { left: 0, top: 0 }
+      const isNavigatingToNestedRoute = to.matched.some((toRoute, index) => {
+        const fromRoute = from.matched[index];
+        return fromRoute && toRoute.path === fromRoute.path;
+      });
+
+      if (!isNavigatingToNestedRoute) {
+        position = { left: 0, top: 0 };
+      }
     }
 
-    // Hash routes on the same page, no page hook is fired so resolve here
     if (to.path === from.path) {
       if (from.hash && !to.hash) {
-        return { left: 0, top: 0 }
+        return { left: 0, top: 0 };
       }
       if (to.hash) {
-        return { el: to.hash, top: _getHashElementScrollMarginTop(to.hash), behavior }
+        return { el: to.hash, top: _getHashElementScrollMarginTop(to.hash), behavior };
       }
-      // The route isn't changing so keep current scroll position
-      return false
+      return false;
     }
 
-    // Wait for `page:transition:finish` or `page:finish` depending on if transitions are enabled or not
-    const hasTransition = (route: RouteLocationNormalized) => !!(route.meta.pageTransition ?? defaultPageTransition)
-    const hookToWait = (hasTransition(from) && hasTransition(to)) ? 'page:transition:finish' : 'page:loading:end'
+    const hasTransition = (route: RouteLocationNormalized) => !!(route.meta.pageTransition ?? defaultPageTransition);
+    const hookToWait = (hasTransition(from) && hasTransition(to)) ? 'page:transition:finish' : 'page:loading:end';
     return new Promise((resolve) => {
       nuxtApp.hooks.hookOnce(hookToWait, () => {
-        requestAnimationFrame(() => resolve(_calculatePosition(to, 'instant', position)))
-      })
-    })
+        requestAnimationFrame(() => resolve(_calculatePosition(to, 'instant', position)));
+      });
+    });
   },
-}
+};
 
 function _getHashElementScrollMarginTop (selector: string): number {
   try {
